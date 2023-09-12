8eb83390ad1d9b3911dbf458632b6dafe4d67cd2
FROMERO63 <romerofrank63@gmail.com>
2023-09-11 18:32:25 -0700

    working on the shapes

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+const { error } = require('console');
 const fs = require('fs');
 const inquirer = require('inquirer');
+const {Circle, Square, Triangle} = require("./lib/shapes.js");
+{}
+class Svg{
+    constructor(){
+        this.textElement =''
+        this.shapeElement = ''
+    }
+    render(){
+        return `<svg version="1.1 xmlns="http://wwww.w3.org/2000/svg" width="300" height="200"/>`
+    }
+    setTextElement(text,color){
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${textcolor}">${text}</text>`
+    }
+}
 
-
-const questions = [
+questions =    [
     {
         type: 'input',
         message: 'Please enter the 3 characters to represent your logo',
@@ -11,17 +25,35 @@ const questions = [
     {
         type: 'input',
         message: 'Please enter color for logo text',
-        name: 'logocolor'
+        name: 'textcolor'
     },
     {
         type: 'list',
         message: 'Please choose a shaoe from below',
         name: 'shape',
-        choices: ['square','circle','triangle']
+        choices: ['Square','Circle','Triangle']
     },
     {
         type: 'input',
         message: 'Please enter color for logo shape',
-        name: 'shapecolor'
+        name: 'logocolor'
     }
 ]
+
+
+
+// function writeToFile (fileName, data){
+//     return fs.writeFileSync(path.join(process.cwd(),fileName),data);
+// }
+
+function logoChoices(){
+    inquirer.prompt(questions).then((responses)=> {
+    console.log(responses);
+    console.log("Generated logo.svg");
+    generateLogo();
+  //  writeToFile("logo.sgv", generateLogo({...responses}))
+    })}
+
+
+logoChoices();
+
